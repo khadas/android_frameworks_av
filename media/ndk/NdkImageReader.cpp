d355b3f34da90a6b9ad87d4ece20f6e3c0c5663d
Chen Jinsen <kevin.chen@rock-chips.com>
2023-08-29 11:51:31 +0800

    ndk: remove crop corner limit for afbc output mode
    Signed-off-by: Chen Jinsen <kevin.chen@rock-chips.com>
    Change-Id: I3fde4c7dd08b564d56fcea8fa957e3c495287c60

--- media/ndk/NdkImageReader.cpp
+++ media/ndk/NdkImageReader.cpp
@@ -455,7 +455,7 @@ AImageReader::acquireImageLocked(/*out*/AImage** image, /*out*/int* acquireFence
         Point lt = buffer->mCrop.leftTop();
         if (lt.x != 0 || lt.y != 0) {
             ALOGE("Crop left top corner [%d, %d] not at origin", lt.x, lt.y);
-            return AMEDIA_ERROR_UNKNOWN;
+            // return AMEDIA_ERROR_UNKNOWN;
         }
 
         // Check if the producer buffer configurations match what ImageReader configured.
